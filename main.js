a5ff18d7808dab4ffe6ad27afa69c23ab5d66ab1
bhargav <bhargav.annem@gmail.com>
2023-08-03 18:18:04 -0500

    parallel multiplication

--- main.js
+++ main.js
@@ -4,3 +4,77 @@ const dstHeight = 2;
 const canvas = document.createElement("canvas");
 canvas.width = dstWidth;
 canvas.height = dstHeight;
+
+const vs = `#version 300 es
+in vec4 position;
+void main() {
+  gl_Position = position;
+}
+`;
+
+const fs = `#version 300 es
+precision highp float;
+ 
+uniform sampler2D srcTex;
+ 
+out vec4 outColor;
+ 
+void main() {
+  ivec2 texelCoord = ivec2(gl_FragCoord.xy);
+  vec4 value = texelFetch(srcTex, texelCoord, 0);  // 0 = mip level 0
+  outColor = value * 2.0;
+}
+`;
+
+const gl = canvas.getContext("webgl2");
+
+const program = webglUtils.createProgramFromSources(gl, [vs, fs]);
+const positionLoc = gl.getAttribLocation(program, "position");
+const srcTexLoc = gl.getUniformLocation(program, "srcTex");
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(
+  gl.ARRAY_BUFFER,
+  new Float32Array([-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1]),
+  gl.STATIC_DRAW
+);
+
+const vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+
+gl.enableVertexAttribArray(positionLoc);
+gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+
+const srcWidth = 3;
+const srcHeight = 2;
+const tex = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, tex);
+gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+gl.texImage2D(
+  gl.TEXTURE_2D,
+  0,
+  gl.R8,
+  srcWidth,
+  srcHeight,
+  0,
+  gl.RED,
+  gl.UNSIGNED_BYTE,
+  new Uint8Array([1, 2, 3, 4, 5, 6])
+);
+
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+gl.useProgram(program);
+gl.uniform1i(srcTexLoc, 0);
+gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+const results = new Uint8Array(dstWidth * dstHeight * 4);
+gl.readPixels(0, 0, dstWidth, dstHeight, gl.RGBA, gl.UNSIGNED_BYTE, results);
+
+for (let i = 0; i < dstWidth * dstHeight; i++) {
+  console.log(results[i * 4]);
+}
