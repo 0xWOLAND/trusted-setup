96c0568d692a77ab51026f2f3224fc270d33ae6f
bhargav <bhargav.annem@gmail.com>
2023-08-11 12:20:15 -0500

    pm-solver

--- pm-solver.js
+++ pm-solver.js
@@ -1,9 +1,9 @@
 const WIDTH = 500;
 const HEIGHT = 500;
 
-const H = 1;
 const PARTICLE_MASS = 5.0;
 const PARTICLE_SIZE = 3.0;
+const G = 6.6743 * 1e-11;
 
 function solve(g) {
   let M = new Array(WIDTH).fill(new Array(HEIGHT).fill(0));
@@ -12,15 +12,22 @@ function solve(g) {
     const x = (WIDTH * g[i]) / 2 + WIDTH / 2;
     const y = (HEIGHT * g[i + 1]) / 2 + HEIGHT / 2;
 
-    const dx = x - Math.floor(x / H) * H;
-    const dy = y - Math.floor(y / H) * H;
+    for (let _dx = -1; _dx < 1; _dx++) {
+      for (let _dy = -1; _dy < 1; _dy++) {
+        const dx = x - Math.floor(x) + _dx;
+        const dy = y - Math.floor(y) + _dy;
 
-    const m = ((1 - dx / H) * (1 - dy / H)) / (H * H);
+        const m = (1 - dx) * (1 - dy);
 
-    const _x = Math.floor(x);
-    const _y = Math.floor(y);
+        const _x = Math.floor(x);
+        const _y = Math.floor(y);
 
-    M[_x][_y] = PARTICLE_MASS * m;
+        if (!(0 <= _x && _x < WIDTH) && !(0 <= _y && _y < HEIGHT)) continue;
+
+        M[_x][_y] = PARTICLE_MASS * m;
+      }
+    }
   }
+
   console.log(M);
 }
